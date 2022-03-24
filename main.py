70ddf4f657b5cb4bdb8dc50996f1252a162da040
Ugor Marcilio Brandão Costa <52638444+ubrando@users.noreply.github.com>
2022-03-24 20:11:03 -0300

    change ideia

--- main.py
+++ main.py
@@ -1,139 +1,61 @@
-
-import atexit
-import pygame
-from pygame.locals import *
-from sys import exit
-from pygame.image import load
-from pygame.transform import scale
-from pygame.sprite import Sprite, Group
-from pygame import event
-from random import randint
-import numpy as np
-import math
-import statistics
-
-jogadores = 20
+from cgitb import html
+from tkinter.messagebox import QUESTION
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 
-def dist(a, b):
-    x1 = a.rect.x
-    x2 = b.rect.x
-    y1 = a.rect.y
-    y2 = b.rect.y
-    return math.hypot(x1-x2, y1-y2)
+app = Flask(__name__)  # sempre ao iniciar um site
 
 
-def bruto(x):
-    n = len(x)
-    ponto1 = 0
-    ponto2 = 0
-    distmin = 10000000000000000000000  # inicializar com distância muito grande
-    for i in range(0, n-1):  # comparar todos os pares possíveis
-        for j in range(i+1, n-1):
-            if dist(x[i], x[j]) <= distmin:
-                ponto1 = i
-                ponto2 = j
-                distmin = dist(x[i], x[j])
-                print(ponto1, ponto2, distmin)
-    atletas[ponto1].kill()
-    atletas.pop(ponto1)
-    atletas[ponto2].kill()
-    atletas.pop(ponto2)
+def count_inversion(lst):
+    return merge_count_inversion(lst)[1]
 
 
-pygame.init()
-musica_tema = pygame.mixer.music.load('BoxCat Games - CPU Talk.mp3')
-pygame.mixer.music.play(-1)
-
-largura = 1000
-altura = 600
-tamanhoDaTela = 1000, 600
-
-fonte = pygame.font.SysFont('arial', 40, True, True)
-
-
-tela = pygame.display.set_mode(tamanhoDaTela)
-pygame.display.set_caption('STOP!')
-fundo = scale(
-    load('images/quadra.png'),
-    tamanhoDaTela)
-clock = pygame.time.Clock()
+def merge_count_inversion(lst):
+    if len(lst) <= 1:
+        return lst, 0
+    middle = int(len(lst) / 2)
+    left, a = merge_count_inversion(lst[:middle])
+    right, b = merge_count_inversion(lst[middle:])
+    result, c = merge_count_split_inversion(left, right)
+    return result, (a + b + c)
 
 
-class Atleta(Sprite):
-
-    def __init__(self):
-        super().__init__()
-
-        self.image = scale(
-            load('images/atleta.png'),
-            (60, 60))
-        self.rect = self.image.get_rect(
-            center=(randint(0, 900), randint(0, 500)
-                    ))
-        self.jogo = 0
-        self.direction = 1
-        self.speed_x = 5
-        self.speed_y = 4
-
-    def playGame(self):
-        self.jogo = 1
-
-    def update(self):
-        if(self.jogo == 1):
-            if(self.rect.left <= 20 or self.rect.right >= 940):
-                self.direction *= -1
-                self.speed_y = randint(1, 5) * self.direction
-                self.speed_x = randint(1, 5) * self.direction
-                if self.speed_x == 0 and self.speed_y == 0:
-                    self.speed_x = randint(2, 5) * self.direction
-                    self.speed_y = randint(2, 5) * self.direction
-            if self.rect.top <= 50 or self.rect.bottom >= 540:
-                self.direction *= -1
-                self.speed_y = randint(1, 5) * self.direction
-                self.speed_x = randint(1, 5) * self.direction
-                if self.speed_x == 0 and self.speed_y == 0:
-                    self.speed_x = randint(2, 5) * self.direction
-                    self.speed_y = randint(2, 5) * self.direction
-
-            self.rect.x += self.speed_x
-            self.rect.y += self.speed_y
+def merge_count_split_inversion(left, right):
+    result = []
+    count = 0
+    i, j = 0, 0
+    left_len = len(left)
+    while i < left_len and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            count += left_len - i
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result, count
 
 
-atletas = []
-grupo_atletas = Group()
-i = 0
-while i <= 19:
-    atletas.append(Atleta())
-    grupo_atletas.add(atletas[i])
-    i = i + 1
+@app.route('/', methods=['GET', 'POST'])
+def homepage():
+    if request.method == "POST":
+        listaDePreferencias = []
+        listaDePreferencias.append(request.form["question1"])
+        listaDePreferencias.append(request.form["question2"])
+        listaDePreferencias.append(request.form["question3"])
+        listaDePreferencias.append(request.form["question4"])
+        listaDePreferencias.append(request.form["question5"])
+        return f"{count_inversion(listaDePreferencias)}"
+    else:
+        return render_template("homepage.html")
 
 
-while True:
-    clock.tick(60)
-    mensagem = f'Jogadores: {jogadores}'
-    texto_formatado = fonte.render(mensagem, False, (0, 0, 255))
+@app.route("/<quention1>")
+def teste(question1):
+    return f"<h1>{listaDePreferencias[0]}<h1>"
 
-    # Espaco dos Eventos
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            exit()
-        if event.type == KEYUP:
-            if event.key == K_SPACE:
-                k = 0
-                while k <= 19:
-                    atletas[k].playGame()
-                    k += 1
-            if event.key == K_TAB:
 
-                bruto(atletas)
-                jogadores = jogadores - 2
-
-    # Espaco deo siplay
-    tela.blit(fundo, (0, 0))
-    tela.blit(texto_formatado, (390, 20))
-
-    grupo_atletas.draw(tela)
-    grupo_atletas.update()
-    pygame.display.update()
+if __name__ == "__main__":
+    app.run(debug=True)
